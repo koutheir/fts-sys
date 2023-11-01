341a8149f9e20ec08b61e5fb428d3ec1b594ed29
Koutheir Attouchi <koutheir@gmail.com>
2023-10-31 20:48:14 -0400

    Version 0.2.7.

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,13 @@
 #![doc = include_str!("../README.md")]
 #![cfg(unix)]
-#![doc(html_root_url = "https://docs.rs/fts-sys/0.2.6")]
-#![allow(non_upper_case_globals, non_camel_case_types, non_snake_case)]
-#![allow(clippy::redundant_static_lifetimes)]
+#![doc(html_root_url = "https://docs.rs/fts-sys/0.2.7")]
+#![warn(unsafe_op_in_unsafe_fn)]
+#![allow(
+    non_upper_case_globals,
+    non_camel_case_types,
+    non_snake_case,
+    clippy::redundant_static_lifetimes
+)]
 
 use libc::dev_t;
 use libc::ino_t;
